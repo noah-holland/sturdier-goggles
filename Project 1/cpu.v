9eb0339d27bf153a7b4e6ab785c57e02af8507f6
Ryan Job <ryanjob42@gmail.com>
2018-03-16 16:17:35 -0500

    Finished up a rough draft of cpu.v

--- Project 1/cpu.v
+++ Project 1/cpu.v
@@ -25,7 +25,7 @@ module cpu (
 	input   wire            clk,
 	input   wire            rst_n,
 	output  wire            hlt,
-	output  reg     [15:0]  pc
+	output  wire    [15:0]  pc
 );
 
 
@@ -51,61 +51,66 @@ localparam OPCODE_HLT    = 4'hF;
 wire    [3:0]   opcode;
 
 
-// Lines going in/out of the memory modules
-wire    [15:0]  instruction;            // The value read from instr. memory
+// This is output by the PC register just for the PCS instruction
+wire    [15:0]  pc_plus_two;
 
-wire    [15:0]  data_mem_data_out;		// The value read from data memory
-reg     [15:0]  data_mem_data_in;       // The value to write to data memory
-reg     [15:0]  data_mem_addr;          // The address to read from or write to
-reg             data_mem_enable;        // Enables memory reading and writing
-reg             data_mem_wr;            // Enables memory writing. Requires
-                                        //     data_mem_enable to be asserted
+
+// This is the only thing not directly mapped in the instruction memory. It's
+// just the current instruction
+wire    [15:0]  instruction;
+
 
 // Lines going in/out of the register file
-reg     [3:0]   src_reg_1;              // The first register to read from
-reg     [3:0]   src_reg_2;              // The second register to read from
-reg     [3:0]   dest_reg;               // The register to write to
-reg             reg_write;              // Reg file write enable
-reg     [15:0]  reg_write_data;         // The value to write to dest_reg
+wire    [3:0]   src_reg_1;              // The first register to read from
+wire    [3:0]   src_reg_2;              // The second register to read from
+wire    [3:0]   dest_reg;               // The register to write to
+wire            reg_write;              // Reg file write enable
+wire    [15:0]  reg_write_data;         // The value to write to dest_reg
 wire    [15:0]  src_data_1;             // The value read from src_reg_1
 wire    [15:0]  src_data_2;             // The value read from src_reg_2
 
 
 // Lines going in/out of the ALU (some are manually assigned)
-wire    [15:0]  alu_in_2;
-wire    [15:0]  alu_out;
+wire    [15:0]  alu_result;
+wire    [15:0]  flags;
+
+
+// Lines going in/out of the data memory
+wire    [15:0]  data_mem_data_out;		// The value read from data memory
+wire            data_mem_wr;            // Enables memory writing. Requires
+                                        //     data_mem_enable to be asserted
 
 
   //////////////////////////////////////////////////////////////////////////////
  // Internal Modules
 ////////////////////////////////////////////////////////////////////////////////
 
-// The single cycle data memory
-// The memory module was provided to us
-memory1c data_mem (
-	.data_out	(data_mem_data_out),
-	.data_in    (data_mem_data_in),
-	.addr       (data_mem_addr),
-	.enable     (data_mem_enable),
-	.wr         (data_mem_wr),
-	.clk        (clk),
-	.rst        (~rst_n)
+// The PC register module, which is in charge of maintaining the current PC
+// and updating it each clock cycle
+pc_register pc_register_instance (
+	.clk            (clk),
+	.rst_n          (rst_n),
+	.instruction    (instruction),
+	.branch_reg_val (src_reg_1),
+	.flags          (flags),
+	.pc             (pc),
+	.pc_plus_two    (pc_plus_two)
 );
 
 // The single cycle instruction memory
 // The memory module was provided to us
-memory1c inst_mem (
+memory1c memory1c_instruction_instance (
 	.data_out	(instruction),
 	.data_in    (16'h0000),     // Don't need to write ever
 	.addr       (pc),
-	.enable     (1'b1),         // Always can read
+	.enable     (~hlt),         // Always read until hlt is asserted
 	.wr         (1'b0),         // Don't need to write ever
 	.clk        (clk),
 	.rst        (~rst_n)
 );
 
 // The register file we made
-register_file reg_file (
+register_file register_file_instance (
 	.clk        (clk),
 	.rst        (~rst_n),
 	.SrcReg1    (src_reg_1),
@@ -118,40 +123,65 @@ register_file reg_file (
 );
 
 // The ALU module for addition/subtraction
-alu alu_adder (
-	.alu_in_1   (src_data_1),
-	.alu_in_2   (alu_in_2),
+alu alu_instance (
+	.src_data_1 (src_data_1),           // The register value from src_reg_1
+	.src_data_2 (src_data_2),           // The register value from src_reg_2
+	.immediate  (instruction[3:0]),     // The 4-bit immediate value
 	.opcode     (opcode),
-	.alu_out    (alu_out)
+	.alu_result (alu_result),
+	.flags      (flags)
+);
+
+// The single cycle data memory
+// The memory module was provided to us
+memory1c memory1c_data_instance (
+	.data_out	(data_mem_data_out),
+	.data_in    (src_reg_2),            // src_reg_2 is the only thing stored
+	.addr       (alu_result),           // The address always comes from the ALU
+	.enable     (~hlt),                 // Always read until hlt is asserted
+	.wr         (data_mem_wr),
+	.clk        (clk),
+	.rst        (~rst_n)
 );
 
 
   //////////////////////////////////////////////////////////////////////////////
- // Register File Control
+ // Not Register File Control Stuff
 ////////////////////////////////////////////////////////////////////////////////
 
-// This just makes it easier to write this code
+// This just makes it easier to write stuff
 assign opcode = instruction[15:12];
+
+// If the current instruction is HLT, then assert hlt. Since the opcode for
+// HLT is 4'hF, I can just use an AND reduction on the opcode
+assign hlt = &opcode;
+
+// Only write to memory for SW instructions
+assign data_mem_wr = (opcode == OPCODE_SW) ? 1'b1 : 1'b0;
+
+
+  //////////////////////////////////////////////////////////////////////////////
+ // Register File Control Stuff
+////////////////////////////////////////////////////////////////////////////////
 
 // To learn more about the register assignments, see "instruction_encoding.txt"
 
-// All instructions except SW use instruction[7:4] as src_reg_1 (if they use
-// it at all). If it's not needed, then it doesn't matter what's being read
-assign src_reg_1 = (opcode == OPCODE_SW) ? instruction[11:8] : instruction[7:4];
+// All instructions use instruction[7:4] as src_reg_1 (if they use it at all).
+// If it's not needed, then it doesn't matter what's being read
+assign src_reg_1 = instruction[7:4];
 
-// Compute instructions use instruction[3:0]. SW uses instruction[7:4]. No
-// other instruction uses src_reg_2, so it can just be whatever.
+// Compute instructions use instruction[3:0]. SW uses instruction[11:8].
+// No other instruction uses src_reg_2, so it can just be whatever.
 // If opcode[3] == 1'b0, then it's a compute instruction.
-assign src_reg_2 = (opcode[3] == 1'b0) ? instruction[3:0] : instruction[7:4];
+assign src_reg_2 = (opcode[3] == 1'b0) ? instruction[3:0] : instruction[11:8];
 
 // There are no instructions where dest_reg is not instruction[11:8]. Thus,
 // it'll just be up to the reg_write signal to make sure writes don't screw
 // things up
 assign dest_reg = instruction[11:8];
 
-// Only write to registers for certain instructions. These instructions are:
-//	- All compute instructions (opcode[3] == 1'b0)
-//	- LW, LHB, LLB, PCS
+// Only write to registers for certain instructions. These instructions are
+// LW, LHB, LLB, PCS, and all compute instructions (opcode[3] == 1'b0).
 // In order to simplify the logic, I used a Karnaugh map to determine a Sum of
 // Products solution to this logic
 assign reg_write =
@@ -166,8 +196,9 @@ assign reg_write_data =
 	(opcode == OPCODE_LW)  ? data_mem_data_out :
 	(opcode == OPCODE_LHB) ? {instruction[7:0], 8'h00} :
 	(opcode == OPCODE_LLB) ? {8'h00, instruction[7:0]} :
-	(opcode == OPCODE_PCS) ? pc_increment;
+	(opcode == OPCODE_PCS) ? pc_plus_two :
 	                         alu_out;
+
 
 endmodule
 
--- README.md
+++ README.md
@@ -11,6 +11,9 @@ Mark with `[initial]` when in progress change to `[x]` when completed. Feel free
 
 ##### ALU
 - [ ] alu.v
+	- Notes from Ryan for whoever works on alu.v:
+		- For LW and SW instructions, the operation should perform: src_data_1 + (immediate << 1) to calculate the memory address
+		- Implementing the control logic inside the ALU would be best in my opinion, but whoever works on it can do whatever
 - [x] CLA
 - [N] Saturation module
 - [ ] RED
@@ -19,10 +22,18 @@ Mark with `[initial]` when in progress change to `[x]` when completed. Feel free
 
 ##### Memory
 - [x] LWSW
+	- Done in cpu.v
 - [x] LHB/LLB (may be similar)
+	- Done in cpu.v
 
 ##### Control
 - [ ] General cotnrol
+- [R] pc\_register.v
+	- Includes a register to store the PC
+	- Includes logic to update the PC each clock cycle
 - [R] Branch/Branch Register
+	- Done in pc\_register.v
 - [x] PCS
+	- Done in cpu.v
 - [R] HLT
+	- Done in pc\_register.v
