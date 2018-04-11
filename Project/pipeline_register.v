1d55038a5fcf0a6f7ab1158be996e3f3c0ea5b45
Noah Holland <moah909@gmail.com>
2018-04-11 01:00:47 -0500

    Fixed swapped inputs and outputs

--- Project/pipeline_register.v
+++ Project/pipeline_register.v
@@ -2,7 +2,7 @@ module pipeline_register (input stall, flush, clk, [3:0] opcode_in, [63:0] input
 
 wire [67:0] internal_outputs;
 
-dff FFs[67:0] (.clk(clk), .q({opcode_in, inputs}), .d(internal_outputs), .wen(~stall), .rst(flush));
+dff FFs[67:0] (.clk(clk), .d({opcode_in, inputs}), .q(internal_outputs), .wen(~stall), .rst(flush));
 
 // If we are stalling we want to output all 0's
 assign {opcode_out, outputs} = stall ? 67'h0 : internal_outputs;
